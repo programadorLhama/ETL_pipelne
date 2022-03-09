c91c58aed4d802b83fdb8d60d1686959301e6518
programadorLhama <programadorLhama@gmail.com>
2022-03-08 22:34:38 -0300

    feat: Implementing HttpRequester

---
+++ src/drivers/http_requester.py
@@ -0,0 +1,14 @@
+from typing import Dict
+import requests
+
+class HttpRequester:
+
+    def __init__(self) -> None:
+        self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
+
+    def request_from_page(self) -> Dict[int, str]:
+        response = requests.get(self.__url)
+        return {
+            "status_code": response.status_code,
+            "html": response.text
+        }
---
+++ src/drivers/http_requester_test.py
@@ -0,0 +1,14 @@
+from .http_requester import HttpRequester
+
+def test_request_from_page(requests_mock):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
+    response_context = '<h1>OlaMundo</h1>'
+    requests_mock.get(url, status_code=200, text=response_context)
+
+    http_requester = HttpRequester()
+    request_response = http_requester.request_from_page()
+
+    assert 'status_code' in request_response
+    assert 'html' in request_response
+    assert request_response["status_code"] == 201
+    assert request_response["html"] == response_context
